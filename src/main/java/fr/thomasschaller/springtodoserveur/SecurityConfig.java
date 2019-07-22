5d54acc04c61adecd4255db2f81a33d847fa0761
Thomas Schaller <41308408+thomas-schaller@users.noreply.github.com>
2019-07-22 14:39:07 +0200

    Modification de la sécurité pour authoriser l'authentification basique et authorisé les appels Rest à partir de localhost:4200.

--- src/main/java/fr/thomasschaller/springtodoserveur/SecurityConfig.java
+++ src/main/java/fr/thomasschaller/springtodoserveur/SecurityConfig.java
@@ -1,22 +1,22 @@
 package fr.thomasschaller.springtodoserveur;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
 @EnableWebSecurity
+@CrossOrigin(origins = "http://localhost:4200")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
-/*
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     }
-*/
+
     @Bean
     public DaoAuthenticationProvider authenticationProvider(UserDetailsServiceDAO userDetailsService) {
         final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
